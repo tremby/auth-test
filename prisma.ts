d72fe58927e2dcfcf19c08af0f9740ab061d5f42
Bart Nagel <bart@tremby.net>
2025-04-16 18:07:16 -0700

    Configure neon serverless as per prisma docs
    An attempt to follow https://www.prisma.io/docs/orm/overview/databases/neon#how-to-use-neons-serverless-driver-with-prisma-orm-preview
    
    The same errors still appear.

--- prisma.ts
+++ prisma.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from "@prisma/client"
+import { PrismaNeon } from "@prisma/adapter-neon"
+
+const connectionString = `${process.env.DATABASE_URL}`
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const adapter = new PrismaNeon({ connectionString });
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
