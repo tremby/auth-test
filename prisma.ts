f15fb9c8f1b135296054c7fa4b63b056a3e19503
Bart Nagel <bart@tremby.net>
2025-04-16 18:12:13 -0700

    Attempt to use neon serverless over websockets
    Attempting to follow https://neon.tech/docs/serverless/serverless-driver#use-the-driver-over-websockets
    
    The same errors still appear.

--- prisma.ts
+++ prisma.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client"
-import { PrismaNeon } from "@prisma/adapter-neon"
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+import ws from 'ws';
 
-const connectionString = `${process.env.DATABASE_URL}`
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-
-const adapter = new PrismaNeon({ connectionString });
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+export const prisma = new PrismaClient({ adapter });
