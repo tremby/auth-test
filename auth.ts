7507b6960a9fe7a7c15f2905ce7dce3508fbac54
Bart Nagel <bart@tremby.net>
2025-04-16 18:00:34 -0700

    Set up database
    Following https://authjs.dev/getting-started/adapters/prisma but
    ignoring the "edge compatibility" section.
    
    As of this commit, trying to load the app results in lots of errors in
    the console:
    
        [Error: Failed to load external module node:module: ReferenceError: Cannot access 'require' before initialization]

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/prisma"
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
 })
