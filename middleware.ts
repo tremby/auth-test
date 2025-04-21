b91706c105a1c8c9518672021024d2647b017e8a
Bart Nagel <bart@tremby.net>
2025-04-21 15:46:37 -0700

    Add next part of gmsetiwan's solution
    See https://github.com/nextauthjs/next-auth/issues/12881#issuecomment-2816971424

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,8 @@
-export { auth as middleware } from "@/auth"
+// middleware.ts
+// This file is used to configure NextAuth.js middleware
+// and protect routes in the application.
+
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+
+export const { auth: middleware } = NextAuth(authConfig);
